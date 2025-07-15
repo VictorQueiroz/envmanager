ca2340996096047d97918d7c0d1b5a090a684924
Victor Queiroz <victorcqueirozg@gmail.com>
2025-07-15 12:06:55 -0300

    Use `/tmp/user-0001` as the default prefix install instead of `/tmp`

--- 00-defaults.sh
+++ 00-defaults.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+envmanager_defaults_user_install_prefix="/tmp/user-0001"
+
 if [ -n "$XDG_DATA_HOME" ]; then
   USER_INSTALL_PREFIX="$XDG_DATA_HOME"
 elif [ -n "$XDG_CONFIG_HOME" ]; then
@@ -8,8 +10,8 @@ elif [ -n "$HOME" ]; then
   USER_INSTALL_PREFIX="$HOME"
 else
   printf 'Unable to determine parent directory for user-level installations. '
-  printf 'Defaulting to /tmp\n'
-  USER_INSTALL_PREFIX="/tmp"
+  printf 'Defaulting to %s\n' "$envmanager_defaults_user_install_prefix"
+  USER_INSTALL_PREFIX="$envmanager_defaults_user_install_prefix"
 fi
 
 export USER_INSTALL_PREFIX

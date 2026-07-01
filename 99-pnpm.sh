24df3d9a2a910326ecba17d1e55600afe89ec4f6
Victor Queiroz <victorcqueirozg@gmail.com>
2026-07-01 09:23:09 -0300

    add support for PNPM version 8

--- 99-pnpm.sh
+++ 99-pnpm.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 # PNPM version
-PNPM_VERSION=11.9.0
+PNPM_VERSION="${PNPM_VERSION:-11.9.0}"
 export PNPM_VERSION
 
 # PNPM home directory
 PNPM_HOME="$XDG_DATA_HOME/pnpm"
 export PNPM_HOME
 
-PATH="$PNPM_HOME/bin:$PATH"
+# 10.23.0
+PATH="$PNPM_HOME:$PATH"
+# 11.9.0
+# PATH="$PNPM_HOME/bin:$PATH"
 export PATH
 
 _install_pnpm() {
   unset -f _install_pnpm
 
-  if command -v pnpm &>/dev/null; then
+  if command -v "$PNPM_HOME"/pnpm &>/dev/null; then
     return 0
   fi
 
